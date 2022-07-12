5658d5f9b013511d27b6968df7da9522851b172e
Carlos Boyzo <carlos.boor@gmail.com>
2022-07-12 04:00:57 -0500

    no image fix

--- components/RecentPosts.jsx
+++ components/RecentPosts.jsx
@@ -23,37 +23,33 @@ const RecentPosts = () => {
   return (
     <>
       <section className='bg-cyan-600 text-white min-h-screen flex flex-col p-7 items-center justify-center'>
-        <h2 className=' py-6 text-2xl font-bold text-center mb-3 '>
-        Más relevantes
+        <h2 className=' py-6 text-4xl font-bold text-center mb-3 '>
+          Más relevantes
         </h2>
         <div className='grid lg:grid-cols-3  gap-6 grid-flow-row-dense sm:grid-cols-1 '>
           {posts.map((post) => {
             console.log(post);
             const imageThumbnail =
               post.featured_media === null
-                ? '/img/no_images.png'
-                : post.featured_media.thumbnail;
-            
+                ? '/no_images.png'
+                : post.featured_media.medium;
+
             const { title, excerpt, link } = post;
             return (
               <Link href={`/posts/${post.id}`} passHref key={post.id}>
-              
-              <div className=" hover:cursor-pointer flex flex-col" >
-                <Image
-                  src={imageThumbnail}
-                  // src={post.featured_media.thumbnail}
-                  alt={post.slug}
-                  
-                  height={300}
-                  width={150}
-                  className={'image rounded-lg'}
-                
-                />
-                <h3 className='hover:text-rose-300 hover:cursor-pointer'>{title}</h3>
-
-              
-               
-              </div>
+                <div className=' hover:cursor-pointer flex flex-col'>
+                  <Image
+                    src={imageThumbnail}
+                    // src={post.featured_media.thumbnail}
+                    alt={post.slug}
+                    height={300}
+                    width={150}
+                    className={'image rounded-lg'}
+                  />
+                  <h3 className='hover:text-cyan-300 hover:cursor-pointer titulos'>
+                    {title}
+                  </h3>
+                </div>
               </Link>
             );
           })}
--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,62 +1,112 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Moment from "moment";
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import Moment from 'moment';
+import Image from 'next/image';
+import Head from 'next/head';
 export const getStaticPaths = async () => {
-  const res = await fetch('https://beta.mejorconsalud.com/wp-json/mc/v3/posts?orderby=date&order=desc')
-  const posts = await res.json()
- 
-  const paths = posts.data.map(post => ({
+  const res = await fetch(
+    'https://beta.mejorconsalud.com/wp-json/mc/v3/posts?orderby=date&order=desc'
+  );
+  const posts = await res.json();
+
+  const paths = posts.data.map((post) => ({
     params: {
-      id: post.id.toString()
-    }
-  }))
+      id: post.id.toString(),
+    },
+  }));
   return {
     paths,
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async ({ params }) => {
-  const res = await fetch(`https://beta.mejorconsalud.com/wp-json/mc/v3/posts/${params.id}`)
-  const post = await res.json()
+  const res = await fetch(
+    `https://beta.mejorconsalud.com/wp-json/mc/v3/posts/${params.id}`
+  );
+  const post = await res.json();
   return {
     props: {
-      post
-    }
-  }
-}
+      post,
+    },
+  };
+};
 
-
-function PagePost({post} ) {
+function PagePost({ post }) {
   return (
+    <>
+      <Head>
+        <title> {`${post.metas.title} - Carlos BOYZO OREGON`}</title>
+        <meta name='description' content={post.metas.description} />
+      </Head>
+      <div>
+        <Navbar />
+        <section>
+          <div className='container mx-auto px-4'>
+            <h1 className='py-4 text-4xl font-bold text-rose-300 text-center'>
+              {post.title}
+            </h1>
+            <picture>
+              <img
+                src={post.featured_media.medium_large}
+                alt={post.slug}
+                className='rounded-lg w-full h-100'
+              />
+            </picture>
+            <div className='flex flex-col md:flex-row gap-5 py-5'>
+              <div className='w-full md:w-1/1'>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+              </div>
+              <div className='w-full md:w-1/4 text-xs'>
+                <span>
+                  {' '}
+                  <strong>Categoria:</strong> {post.categories[0].name}
+                </span>
+                <p>
+                  <span>
+                    <strong>Autor:</strong> {post.author?.name}
+                  </span>
+                </p>
 
-    <div >
-      <Navbar   />
-      <section>
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row gap-5">
-            <div className="w-full md:w-1/2">
-              <h1 className="text-3xl font-bold">{post.title}</h1>
-              <span> Categoria: {post.categories[0].name}</span>
-              <p><div>Autor: {post.author?.name}</div></p>
-              
-             
-              <span >Ultima Actualizacion :</span>
-              {Moment(post.modified).format("DD-MM-YYYY")}
-              <picture>
-              <img src={post.featured_media.thumbnail} alt={post.slug} className="rounded-lg w-full h-100" />
-              </picture>
-            </div>
-            <div className="w-full md:w-1/2">
-              
-              <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                <span>Ultima Actualizacion :</span>
+                {Moment(post.modified).format('DD-MM-YYYY')}
+
+                <div className='contenedor'>
+                  <div className='flex flex-col md:flex-row gap-5 justify-center '>
+                    <div>
+                      {post.author && (
+                        <picture>
+                          <img
+                            className='rounded-lg '
+                            src={post.author?.picture}
+                            alt={post.author?.name}
+                          />
+                        </picture>
+                      )}
+                    </div>
+                  </div>
+                  <br />
+                  <div>
+                    <h5>
+                      {' '}
+                      <strong>Biografia:</strong>{' '}
+                      <span> {post.author?.name}</span>{' '}
+                    </h5>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: post.author?.description,
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-     </section>
-
-    </div>
-  )
+        </section>
+        <section></section>
+      </div>
+    </>
+  );
 }
 
-export default PagePost
+export default PagePost;
